0c20cda60f422dfb971e045ab8e4e413acd7a13c
Szakalski <szakaalski@gmail.com>
2013-04-04 14:00:01 +0200

    New aproach for light networking

--- UnityProject/Assets/Scripts/DrawEmptyScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class DrawEmptyScript : MonoBehaviour {
-	void OnDrawGizmos()	{
-	  Gizmos.color = Color.blue;
-	  Gizmos.DrawWireSphere(transform.position, 0.1f);
-	}
-}
