a4f4a4e282ebc286c947d2d0c998c8d2b64aacef
Szakalski <szakaalski@gmail.com>
2013-04-05 20:59:49 +0200

    Simple GUI + Server List

--- UnityProject/Assets/Scripts/Networking.cs
+++ UnityProject/Assets/Scripts/Networking.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Networking : MonoBehaviour {
 	
-	string IPServer = "127.0.0.1";
-	string IPServ = "";
-	float timeHostWasRegistered;
+	enum MenuState{Main, CreateServer, Connect, ServerGame, ClientGame};
+	
+	string serverName = "Server Name";
+	float timeHostWasRegistered;	
+	Rect windowRect = new Rect(Screen.width * .5f-150f, 20f, 300f, 150f);
+	MenuState guiState=MenuState.Main;
 	
 	void Awake () {
         MasterServer.ClearHostList();
@@ -13,66 +16,35 @@ public class Networking : MonoBehaviour {
 	}
 	
 	void OnGUI () {		
-		if (Network.peerType == NetworkPeerType.Disconnected) {
-			IPServer = GUI.TextField(new Rect(120,10,100,20),IPServer);
-			
-			if (GUI.Button (new Rect(10,10,100,30),"Conect"))
-		 		Network.Connect(IPServer, 25000);	
-			
-			if (GUI.Button (new Rect(10,50,100,30),"Start Server")) {
-				Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
-	  			MasterServer.RegisterHost("GodlyCubesLight", "Godly Cubes Testserver", "Testing GodlyCubes" );
+		if (guiState==MenuState.Main) {
+			if (GUI.Button (new Rect(Screen.width * .5f-120f, 20f, 100f, 50f),"Create Server"))
+				guiState = MenuState.CreateServer;
+				
+			if (GUI.Button (new Rect(Screen.width * .5f+20f, 20f, 100f, 50f),"Connect")) 
+				guiState = MenuState.Connect;
+		}
+		
+ 		if(guiState==MenuState.CreateServer) { 
+			serverName = GUI.TextField(new Rect(Screen.width * .5f-50f, 20f, 100f, 20f), serverName);
+			if (GUI.Button (new Rect(Screen.width * .5f-50f, 50f, 100f, 50f),"Create")) {
+  				Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
+	  			MasterServer.RegisterHost("GodlyCubesLight", serverName);
 				timeHostWasRegistered = Time.time;
 		 		foreach (GameObject go in FindObjectsOfType(typeof(GameObject))) {
 		 			 go.SendMessage("OnLoaded", SendMessageOptions.DontRequireReceiver);	
 				}
+				guiState=MenuState.ServerGame;
 			}
-	 	} 
- 		else{  
-  			if(Network.isServer) {
-				IPServ = Network.player.ipAddress;
-				GUI.Label(new Rect(140,20,250,40),"IP to be used: "+IPServ );
-  			}    
+			if (GUI.Button (new Rect(Screen.width * .5f-50f, Screen.height - 70f, 100f, 50f),"Back"))
+				guiState = MenuState.Main;
+		}	
 			
-  			if (GUI.Button (new Rect(10,10,100,30),"Exit")){
-				
-   				if(Network.isServer)
-   					networkView.RPC("ExitCL", RPCMode.Others);
-   				
-				Network.Disconnect();
-  				Application.Quit();
-  			}
- 		}
-		
-		if (Time.time - timeHostWasRegistered >= 1.0f)
-		{
-			MasterServer.RequestHostList("GodlyCubesLight");
-		}
-		HostData[] data = MasterServer.PollHostList();
-		// Go through all the hosts in the host list
-		foreach (var element in data)
-		{
-			GUILayout.BeginHorizontal();	
-			var name = element.gameName + " " + element.connectedPlayers + " / " + element.playerLimit;
-			GUILayout.Label(name);	
-			GUILayout.Space(5);
-			string hostInfo;
-			hostInfo = "[";
-			foreach (var host in element.ip)
-				hostInfo = hostInfo + host + ":" + element.port + " ";
-			hostInfo = hostInfo + "]";
-			GUILayout.Label(hostInfo);	
-			GUILayout.Space(5);
-			GUILayout.Label(element.comment);
-			GUILayout.Space(5);
-			GUILayout.FlexibleSpace();
-			if (GUILayout.Button("Connect"))
-			{
-				// Connect to HostData struct, internally the correct method is used (GUID when using NAT).
-				Network.Connect(element);			
-			}
-			GUILayout.EndHorizontal();	
-		}		
+		if(guiState==MenuState.Connect) {  
+			windowRect = GUI.Window (0, windowRect, ServerList, "Server List");
+			
+			if (GUI.Button (new Rect(Screen.width * .5f-50f, Screen.height - 70f, 100f, 50f),"Back"))
+				guiState = MenuState.Main;
+		}			
 	}
 	
 	void OnConnectedToServer() {	 
@@ -83,5 +55,35 @@ public class Networking : MonoBehaviour {
 	[RPC]
 	void ExitCL() {
   		Application.Quit();
+	}
+	
+	void ServerList (int windowID) {
+		if(Time.time - timeHostWasRegistered >= 1.0f)
+			{
+				MasterServer.RequestHostList("GodlyCubesLight");
+			}
+			HostData[] data = MasterServer.PollHostList();
+		// Go through all the hosts in the host list
+			foreach (var element in data)
+			{
+				GUILayout.BeginHorizontal();	
+				var name = element.gameName + " " + element.connectedPlayers + " / " + element.playerLimit;
+				GUILayout.Label(name);	
+				GUILayout.Space(5);
+				string hostInfo;
+				hostInfo = "[";
+				foreach (var host in element.ip)
+					hostInfo = hostInfo + host;
+				hostInfo = hostInfo + "]";
+				GUILayout.Label(hostInfo);	
+				GUILayout.Space(5);
+				GUILayout.FlexibleSpace();
+				if (GUILayout.Button("Connect"))
+				{
+					// Connect to HostData struct, internally the correct method is used (GUID when using NAT).
+					Network.Connect(element);			
+				}
+				GUILayout.EndHorizontal();	
+			}
 	}
 }
