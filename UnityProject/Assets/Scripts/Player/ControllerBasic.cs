702cfe200a488fb6f0622bfdad77e4260c979dd6
Burzok <lukasz1burzynski@gmail.com>
2013-04-24 22:18:09 +0200

    working movement

--- UnityProject/Assets/Scripts/Player/ControllerBasic.cs
+++ UnityProject/Assets/Scripts/Player/ControllerBasic.cs
@@ -2,31 +2,52 @@ using UnityEngine;
 using System.Collections;
 
 public class ControllerBasic : MonoBehaviour {
-	
 	public float horizontalInput = 0f;
 	public float verticalInput = 0f;
 	public float rotationInput = 0f;
-	public float forwardSpeed = 5f; 
+	public float forwardSpeed = 20f; 
 	public float rotateSpeed = 250f;
 	
-
-	void Start() {
+	private GameObject[] mainCam;
+	
+	void Awake() {
+		if(!networkView.isMine) {
+			rigidbody.isKinematic = true;
+			rigidbody.useGravity = false;
+			
+			if(!Network.isServer)
+				TurnOffMainCameras();
+		}
+		else {
+			TurnOffMainCameras();
+			transform.FindChild("Camera").gameObject.SetActive(true);
+		}
+	}
+	
+	void TurnOffMainCameras() {
+		mainCam = GameObject.FindGameObjectsWithTag(Tags.mainCamera);
+		foreach( GameObject cam in mainCam )
+			cam.SetActive(false);
 	}
 	
 	void FixedUpdate() {
-		horizontalInput = Input.GetAxis("Horizontal");
-		verticalInput = Input.GetAxis("Vertical");
-		rotationInput = Input.GetAxis("Mouse X");
-		
-		//rigidbody.AddForce( direction * Time.deltaTime * forwardSpeed * verticalInput, ForceMode.Acceleration);
-		if ( verticalInput != 0 )
-			transform.Translate( Vector3.forward  * forwardSpeed * verticalInput * Time.deltaTime);
-		if ( horizontalInput != 0 )
-			transform.Translate( Vector3.right * forwardSpeed * horizontalInput * Time.deltaTime);
-		if ( rotationInput != 0 )
-			transform.Rotate( Vector3.up, rotateSpeed * rotationInput * Time.deltaTime);
-	}
-
-	void Update() {
+		if(networkView.isMine) {
+			horizontalInput = Input.GetAxis("Horizontal");
+			verticalInput = Input.GetAxis("Vertical");
+			rotationInput = Input.GetAxis("Mouse X");
+			
+			if ( verticalInput != 0 ) {
+				//transform.Translate( Vector3.forward  * forwardSpeed * verticalInput * Time.deltaTime);
+				rigidbody.AddForce( transform.forward * Time.deltaTime * forwardSpeed * verticalInput, ForceMode.VelocityChange);
+			}
+			
+			if ( horizontalInput != 0 ) {
+				//transform.Translate( Vector3.right * forwardSpeed * horizontalInput * Time.deltaTime);
+				rigidbody.AddForce( transform.right * Time.deltaTime * forwardSpeed * horizontalInput, ForceMode.VelocityChange);
+			}
+			
+			if ( rotationInput != 0 )
+				transform.Rotate( transform.up, rotateSpeed * rotationInput * Time.deltaTime);
+		}
 	}
 }
--- UnityProject/Assets/Scripts/Unapplied/Tags.cs
+++ UnityProject/Assets/Scripts/Unapplied/Tags.cs
@@ -6,4 +6,7 @@ public class Tags : MonoBehaviour {
 	public const string mainLight = "MainLight";
 	public const string gameController = "GameController";
 	public const string enemy = "Enemy";
+	public const string ally = "Ally";
+	public const string mainCamera = "MainCamera";
+	public const string playerCamera = "PlayerCamera";
 }
