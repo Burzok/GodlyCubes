08feeceeba0595946fb417f7eee830d90a0f15fd
Burzok <lukasz1burzynski@gmail.com>
2013-05-21 07:44:57 +0200

    Basic Attack Cooldown

--- UnityProject/Assets/Scripts/Player/Shooting.cs
+++ UnityProject/Assets/Scripts/Player/Shooting.cs
@@ -3,18 +3,34 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour {
 	public Transform bulletPrefab;
+	public float coolDown = 1.2f;
 	
 	private Transform bulletSpawner;
+	private float coolDownTimer;
 	
 	void Awake() {
 		bulletSpawner = this.transform.FindChild("BulletSpawner");
+		coolDownTimer = 5f;
 	}
 	
 	void Update() {
 		if(networkView.isMine) {
-			if(Input.GetMouseButtonDown(0))
-				networkView.RPC("SpawnBullet", RPCMode.Server, networkView.viewID);
+			coolDownTimer += Time.deltaTime;
+			
+			if(Input.GetMouseButton(0))
+				CheckCoolDown();
 		}
+	}
+	
+	private void CheckCoolDown() {
+		if (coolDownTimer >= coolDown) {
+			Shoot();
+			coolDownTimer = 0f;
+		}
+	}
+	
+	private void Shoot() {
+		networkView.RPC("SpawnBullet", RPCMode.Server, networkView.viewID);
 	}
 	
 	[RPC]
