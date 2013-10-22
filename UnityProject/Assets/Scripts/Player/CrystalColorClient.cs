d5ec1edf9e7c6037da7e19c6376ca37b93972b56
Szakalski <szakaalski@gmail.com>
2013-10-22 20:29:14 +0200

    Add: PlayerCrystalColor serverside

---
+++ UnityProject/Assets/Scripts/Player/CrystalColorClient.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalColorClient : MonoBehaviour {
+
+    private Renderer crystalRenderer;
+    private PlayerStats playerStats;
+    private Material crystalMaterial;
+
+    void Awake() {
+        crystalRenderer = transform.Find("Animator").Find("Crystals").GetComponent<Renderer>();
+        playerStats = gameObject.GetComponent<PlayerStats>();
+    }
+
+	void Update () {
+        
+	}
+}
---
+++ UnityProject/Assets/Scripts/Player/CrystalColorServer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalColorServer : MonoBehaviour
+{
+    private PlayerStats playerStats;
+    private CrystalServer crystalServer;
+    private CrystalType crystalType;
+    private bool colorChanged;
+    private int currentValue;
+
+    void Awake() {
+        playerStats = gameObject.GetComponent<PlayerStats>();
+        colorChanged = false;
+        currentValue = 0;
+    }
+
+    void Update() {
+       crystalType = CheckPrimaryStat();
+       if(colorChanged) {
+           networkView.RPC("ChangePlayerCrystalColor", RPCMode.Others, (int)crystalType, networkView.viewID);
+           colorChanged = false;
+       }
+    }
+
+    CrystalType CheckPrimaryStat() {
+        if (playerStats.strength > playerStats.wisdom && playerStats.strength > playerStats.hardness &&
+            playerStats.strength != currentValue) {
+            currentValue = playerStats.strength;
+            colorChanged = true;
+            return CrystalType.Strength;
+        }
+
+        if (playerStats.wisdom > playerStats.strength && playerStats.wisdom > playerStats.hardness &&
+            playerStats.wisdom != currentValue) {
+            currentValue = playerStats.wisdom;
+            colorChanged = true;
+            return CrystalType.Wisdom;
+        }
+
+        if (playerStats.hardness > playerStats.wisdom && playerStats.hardness > playerStats.strength &&
+            playerStats.wisdom != currentValue) {
+            currentValue = playerStats.hardness;
+            colorChanged = true;
+            return CrystalType.Hardness;
+        }
+
+        return 0;
+    }
+
+    [RPC]
+    void ChangePlayerCrystalColor(int aquiredType, NetworkViewID crystalPlayerID) {}
+}
