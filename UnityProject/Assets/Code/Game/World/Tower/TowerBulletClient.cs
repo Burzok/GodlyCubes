d6a48403747b97be4b0f8dc8772db4b442fc3394
Burzok <lukasz1burzynski@gmail.com>
2014-05-29 23:26:56 +0200

    BulletFix

--- UnityProject/Assets/Code/Game/World/Tower/TowerBulletClient.cs
+++ UnityProject/Assets/Code/Game/World/Tower/TowerBulletClient.cs
@@ -1,50 +1,44 @@
 using UnityEngine;
 using System.Collections;
 
-public class TowerBulletClient : MonoBehaviour {
+public class TowerBulletClient : MonoBehaviour 
+{
 	public Transform target;
 	public float bulletSpeed;
 	
 	public bool flag;
 	
-	public float modif;
-	public float mag;
-	
-	void Awake() {
+	void Awake() 
+	{
 		target = null;
 		flag = true;
-		modif = 4f;
-		bulletSpeed = 1f;
+		bulletSpeed = 10f;
 	}
 	
-	public void SetTarget(Transform newTarget) {
+	public void SetTarget(Transform newTarget) 
+	{
 		target = newTarget;
 	}
 	
-	void FixedUpdate() { 
-			BulletMovement();
+	void FixedUpdate() 
+	{
+		BulletMovement();
 	}
 	
-	private void BulletMovement() {  //TODO: this sucks nead a redo
-		if (target != null) {
-			if (flag) {
+	private void BulletMovement() 
+	{ 
+		if (target != null) 
+		{
+			if (flag) 
+			{
 				animation.Stop();
 				animation.CrossFade("TowerBulletShoot");
 				flag = false;
 			}
-			else {
-				Vector3 toPos = target.position;
-				Vector3 fromPos = transform.position;
-				Vector3 vecLenght = new Vector3(fromPos.x+toPos.x, fromPos.y+toPos.y, fromPos.z+toPos.z);
-				
-				mag = vecLenght.magnitude;
-				
-				if ((vecLenght.magnitude) <= 70f)
-					modif = 8f;
-				else if ((vecLenght.magnitude) <= 100f) 
-					modif = 6f;
-				
-				transform.position = Vector3.Lerp(transform.position, toPos, Time.deltaTime * bulletSpeed * modif);
+			else 
+			{
+				transform.LookAt(target);
+				transform.Translate( 0, 0, Time.deltaTime * bulletSpeed);
 			}
 		}
 	}
--- UnityProject/Assets/Code/Game/World/Tower/TowerBulletServer.cs
+++ UnityProject/Assets/Code/Game/World/Tower/TowerBulletServer.cs
@@ -1,89 +1,88 @@
 using UnityEngine;
 using System.Collections;
 
-public class TowerBulletServer : MonoBehaviour {
+public class TowerBulletServer : MonoBehaviour 
+{
 	public Transform target;
 	public float bulletSpeed;
 	public int damage = 30;
 	public NetworkViewID owner;
 	
 	public bool flag;
-	public bool flagflag;
 	public TowerReloaderServer reloder;
-	
-	public float modif;
-	public float mag;
-	
+		
 	public PlayerData targetData;
 	
-	void Awake() {
+	void Awake() 
+	{
 		target = null;
 		owner = networkView.viewID;
 		flag = true;
-		flagflag = false;
-		modif = 4f;
-		bulletSpeed = 1f;
+		bulletSpeed = 40f;
 	}
 	
-	public void SetTarget(Transform newTarget) {
+	public void SetTarget(Transform newTarget) 
+	{
 		target = newTarget;
 		targetData = target.GetComponent<PlayerData>();
 	}
 	
-	public void SetReloader(TowerReloaderServer newReloader) {
+	public void SetReloader(TowerReloaderServer newReloader) 
+	{
 		reloder = newReloader;
 	}
 	
-	void FixedUpdate() {
+	void FixedUpdate() 
+	{
 		BulletMovement();
 	}
 	
-	private void BulletMovement() { //TODO: this sucks nead a redo
-		if (target != null) {
-			if (flag) {
+	private void BulletMovement() 
+	{
+		if (target != null) 
+		{
+			if (flag) 
+			{
 				flag = false;
 			}
-			else {
-				Vector3 toPos = target.transform.position;
-				Vector3 fromPos = transform.position;
-				Vector3 vecLenght = new Vector3(fromPos.x+toPos.x, fromPos.y+toPos.y, fromPos.z+toPos.z);
-				
-				mag = vecLenght.magnitude;
-				
-				if (vecLenght.magnitude <= 70f) 
-					modif = 8f;
-				else if (vecLenght.magnitude <= 100f) 
-					modif = 6f;	
-				
-				transform.position = Vector3.Lerp(transform.position, toPos, Time.deltaTime * bulletSpeed * modif);
+			else 
+			{
+				transform.LookAt(target);
+				transform.Translate( 0, 0, Time.deltaTime * bulletSpeed);
 			}
 		}
 	}
 	
-	void Update() {
+	void Update() 
+	{
 		DestroyIfTargetIsDead();
 	}
 	
-	private void DestroyIfTargetIsDead() {
-		if(target != null)
-			if(!targetData.isAlive)
-				Network.Destroy(this.gameObject);
+	private void DestroyIfTargetIsDead() 
+	{
+		if(target != null && !targetData.isAlive)
+		{
+			Network.Destroy(this.gameObject);
+		}
 	}
 	
-	void OnTriggerEnter(Collider other) {
-		SendHitData(ref other);
+	void OnTriggerEnter(Collider other) 
+	{
+		SendHitData( ref other);
 		DestroyBullet();
 	}
 	
-	private void SendHitData(ref Collider other) {
+	private void SendHitData(ref Collider other) 
+	{
 		object[] data = new object[2];
-		data[0]=damage;
-		data[1]=owner;
-		other.SendMessage("Hit", data);
+		data[0] = damage;
+		data[1] = owner;
+		other.SendMessage( "Hit", data);
 	}
 	
-	private void DestroyBullet() {
-		Network.RemoveRPCs(networkView.viewID); // sprawdzic czy przy braku tego jest jakas roznica
-		Network.Destroy(this.gameObject);
+	private void DestroyBullet() 
+	{
+		Network.RemoveRPCs( networkView.viewID);
+		Network.Destroy( this.gameObject);
 	}
 }
